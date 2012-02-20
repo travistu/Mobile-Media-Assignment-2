df0a97ffc80358760de6a84303fda651b50474aa
Travis Tu <travistutu@gmail.com>
2012-02-19 23:38:38 -0500

    number/review

--- Restaurants/Restaurant.m
+++ Restaurants/Restaurant.m
@@ -12,10 +12,10 @@
     self = [super init];
     if (self) {
         name = @"Morimoto";
-        address = @"88 10th Ave New York, NY 10024";
+        address = @"ADDRESS: 88 10th Ave New York, NY 10024";
         cuisineType = @"Japanese";
-        phonenumber = @"(212) 989-8883";
-        review = @"I've been here a few times and tried the omakase, which is a lot of food.  Love the toro tatare, which I found out about on the Best Thing I Ever Ate. Beautiful fancy restrooms, with a view of cherry blossoms or something like that.";
+        phonenumber = @"PHONE:(212) 989-8883";
+        review = @"REVIEW: I've been here a few times and tried the omakase, which is a lot of food. Love the toro tatare, which I found out about on the Best Thing I Ever Ate. Beautiful fancy restrooms, with a view of cherry blossoms or something like that.";
         
     }
     return self;
